6cdb8703a52e2b5d7d49f5c2134b8ade1fcf42e6
syabdull <syab5050@gmail.com>
2024-08-23 11:09:32 +0530

    Arrays-finding-pairs

---
+++ script.js
@@ -0,0 +1,9 @@
+//finding out all pairs in an array
+
+let arr = [2, 3, 5, 6, 7];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    console.log(arr[i] + "," + arr[j]);
+  }
+}
